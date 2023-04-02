cd270eb13bd11331a5eaac6e3ef0430388febc01
Kechillosh <kechillosh@gmail.com>
2023-04-02 04:12:44 +0300

    da

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,20 @@ import { SiGooglehome } from "react-icons/si";
 import { SiGooglemessages } from "react-icons/si";
 import { TfiAlignJustify } from "react-icons/tfi";
 import DropdownMenu from "./DropdownMenu";
+import { GoogleMap, LoadScript, MapContext } from "@react-google-maps/api";
+import Map from "./map";
+
 function Navbar() {
+  const handleButtonClick = () => {
+    // Navigate to the desired link
+    window.location.href = "/";
+
+    // Reload the page after the navigation is complete
+    window.addEventListener("load", () => {
+      window.location.reload();
+    });
+  };
+
   return (
     <div className="navbar">
       <div className="leftSide">
@@ -16,13 +29,13 @@ function Navbar() {
         </Link>
       </div>
       <div className="rightSide">
-        <Link className="navLink" to="/">
+        <Link className="navLink" to="/" onClick={handleButtonClick}>
           <SiGooglehome /> Home
         </Link>
         <Link className="navLink" to="/Locations">
           <SiGooglemaps /> Locations
         </Link>
-        <Link className="navLink" to="/Contact">
+        <Link className="navLink" to="/Contact" onClick={handleButtonClick}>
           <SiGooglemessages /> Contact
         </Link>
       </div>
