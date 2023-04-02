61ab6eaaf1003e5cb10f2f1d8288aa752641d729
Kechillosh <kechillosh@gmail.com>
2023-04-02 18:03:55 +0300

    sa

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Instagram, LinkedIn } from "@mui/icons-material";
 import "../styles/Footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <div>
       <div className="footer">
         <div className="socialMedia">
-          <Instagram />
-          <LinkedIn />
+          <a href="https://www.instagram.com/">
+            <Instagram />
+          </a>
+
+          <a href="https://www.linkedin.com/in/amir-el-shaer-b4b6b825a/">
+            <LinkedIn />
+          </a>
+
           <p> &copy; 2023 El Shaer Amir</p>
         </div>
       </div>
