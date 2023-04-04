a77cb57e19f435d52b7b60fad9940f08f602617e
Kechillosh <kechillosh@gmail.com>
2023-04-04 16:05:11 +0300

    test

--- src/Components/map.js
+++ src/Components/map.js
@@ -17,12 +17,10 @@ const containerStyle = {
   height: "100%",
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
-
 function Map() {
+  const [center, setCenter] = useState({ lat: -3.745, lng: -38.523 });
+  const mapRef = React.useRef(null);
+
   const [isMapLoaded, setIsMapLoaded] = useState(true);
 
   const onLoad = (map) => {
@@ -35,47 +33,44 @@ function Map() {
   };
 
   return (
-    <div>
-      <form>
-        <label htmlFor="pickup-location">
-          Pick-up Location <SiGooglemaps />:
-        </label>
-        <Autocomplete>
-          <input
-            type="text"
-            id="pickup-location"
-            name="pickup-location"
-            required
-          />
-        </Autocomplete>
-
-        <label htmlFor="pickup-date">Pick-up Date:</label>
-        <input type="date" id="pickup-date" name="pickup-date" required />
-
-        <label htmlFor="pickup-time">Pick-up Time:</label>
-        <input type="time" id="pickup-time" name="pickup-time" required />
-
-        <label htmlFor="dropoff-date">Drop-off Date:</label>
-        <input type="date" id="dropoff-date" name="dropoff-date" required />
-
-        <input type="submit" value="Calculate Route" />
-      </form>
-
-      <div className="map-container">
-        <LoadScript
-          googleMapsApiKey="AIzaSyBWtJ4FHdXjEY683HopGtSxi-Y3wJezC24"
-          libraries={["places"]}
+    <div className="map-container">
+      <LoadScript
+        googleMapsApiKey="AIzaSyBWtJ4FHdXjEY683HopGtSxi-Y3wJezC24"
+        libraries={["places"]}
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          onLoad={onLoad}
         >
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
-            onLoad={onLoad}
-          >
-            <Marker position={center} />
-          </GoogleMap>
-        </LoadScript>
-      </div>
+          <Marker position={center} />
+        </GoogleMap>
+        <form>
+          <label htmlFor="pickup-location">
+            Pick-up Location <SiGooglemaps />:
+          </label>
+          <Autocomplete>
+            <input
+              type="text"
+              id="pickup-location"
+              name="pickup-location"
+              required
+            />
+          </Autocomplete>
+
+          <label htmlFor="pickup-date">Pick-up Date:</label>
+          <input type="date" id="pickup-date" name="pickup-date" required />
+
+          <label htmlFor="pickup-time">Pick-up Time:</label>
+          <input type="time" id="pickup-time" name="pickup-time" required />
+
+          <label htmlFor="dropoff-date">Drop-off Date:</label>
+          <input type="date" id="dropoff-date" name="dropoff-date" required />
+
+          <input type="submit" value="Calculate Route" />
+        </form>
+      </LoadScript>
     </div>
   );
 }
