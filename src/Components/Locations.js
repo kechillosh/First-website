cb552c266d9e7a4fa07aa442090c6a60cf43e176
Kechillosh <kechillosh@gmail.com>
2023-04-07 16:31:17 +0300

    added some locations

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -1,7 +1,31 @@
+import Tm from "../assets/Piața_Victoriei_Timișoara.jpg";
+import "../styles/Locations.css";
+import Cluj from "../assets/cluj-napoca-1.jpg";
+import Brasov from "../assets/brasov-old-town.jpg";
+import Iasi from "../assets/iasi-romania.jpg";
+
 export const Locations = () => {
   return (
-    <div>
-      <h1>THIS IS THE Locations</h1>
+    <div className="MainLoc">
+      <div>
+        <h1>Top Locations</h1>
+      </div>
+      <div>
+        Timisoara
+        <img src={Tm} alt="Timisoara" />
+      </div>
+      <div className="Cluj">
+        Cluj
+        <img src={Cluj} />
+      </div>
+      <div>
+        Brasov
+        <img src={Brasov} alt="Timisoara" />
+      </div>
+      <div>
+        Iasi
+        <img src={Iasi} alt="Timisoara" />
+      </div>
     </div>
   );
 };
